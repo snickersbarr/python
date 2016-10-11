b32e1f5f824060dd9d9d34f7bd9550baa0fe7dc7
Kunal Pamani <kunalpamani@KP-MBP15.local>
2016-10-11 10:47:19 -0700

    functions.py

--- udemy/functions.py
+++ udemy/functions.py
@@ -26,3 +26,26 @@ def add_num(num1,num2):
 	return num1+num2
 x = add_num(3, 4)
 print x	
+
+print '----------------------------'
+
+# can pass either int or string as arguments
+def add_num(num1,num2):
+	return num1+num2
+x = add_num('one', 'two')
+print x	
+
+"""
+This function checks for prime numbers
+INPUT: A number
+OUTPUT: A print statement whether or not the number is prime
+"""
+def is_prime(num):
+	for n in range(2,num):
+		if num % n == 0:
+			print 'Not Prime'
+			break
+	else:
+		print 'The number is prime'
+is_prime(13)
+is_prime(12)
