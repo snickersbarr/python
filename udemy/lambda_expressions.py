413912609fe71119ad1f1fd1ae989d519021fb0f
Kunal Pamani <kpamani@apple.com>
2016-10-11 19:47:36 -0700

    lambda expressions

---
+++ udemy/lambda_expressions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+## Lambda Expressions ##
+
+# create anonymous functions
+# usually a one line expression
+
+# simple square root function to square a given number
+def square(num):
+	result = num**2
+	return result
+print square(2)
+print square(4)
+
+def
