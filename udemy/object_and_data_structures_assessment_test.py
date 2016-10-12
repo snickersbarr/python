e46f0a3765941b9c8a77d7d883e9a3cef288a545
Kunal Pamani <kunalpamani@KP-MBP15.local>
2016-10-12 00:58:49 -0700

    Oject and Data Structures Assessment

--- udemy/object_and_data_structures_assessment_test.py
+++ udemy/object_and_data_structures_assessment_test.py
@@ -1,12 +1,39 @@
 #!/usr/bin/python
+# from __future__ import division
+# comment above will allow statements below to give correct output
 
 a = 100.25
-print 'a = %s' %(a)
-b = a/2
-print 'b = %s' %(b)
-c = b - 30
-print 'c = %s' %(c)
-d = c + .875
-print 'd = %s' %(d)
-e = d * .33333333
-print 'e = %s' %(e)
+print 'a = ', a
+
+b = 1**2*9/3+98.25-1
+print b 
+
+c = 2/3
+print c
+
+d = (2/3)
+print d
+
+e = 2.0/3.0
+print e
+
+print 4 * (6 + 5)
+print 4 * 6 + 5
+print 4 + 6 * 5
+
+f = 3 + 1.5 + 4
+print type(f)
+
+s = 'hello'
+print s[1]
+print s[::-1]
+print s[4]
+print s[-1]
+
+l = []
+i = 0
+while i < 3:
+	l.append(0)
+	print l
+	i +=1
+	
