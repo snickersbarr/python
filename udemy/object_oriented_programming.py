17024c9fa7b47bc1c26038f81845a450fd837fde
Kunal Pamani <kpamani@apple.com>
2016-10-18 19:30:41 -0700

    More OOP

--- udemy/object_oriented_programming.py
+++ udemy/object_oriented_programming.py
@@ -109,8 +109,21 @@ class Book(object):
 		self.title = title
 		self.author = author
 		self.pages = pages
+	
+	def __str__(self):
+		return "Title: %s, Author: %s, pages %s " %(self.title,self.author,self.pages)
 		
-b = Book('Python','Kunal',100)
+	def __len__(self):
+		return self.pages
+	
+	def __del__(self):
+		print "A book is gone!"
+		
+b = Book('Python','Kunal',101)
 print b.title
 print b.author
 print b.pages
+print b
+print len(b) # Will return the number of pages
+del b
+# b.title #This wont work now because of the __del__ function
