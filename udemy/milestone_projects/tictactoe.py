4184bff40421b9091bf1bac978f1007ecd4f392c
Kunal Pamani <kunalpamani@KP-MBP15-Personal.local>
2016-10-21 00:03:04 -0700

    Tic Tac Toe Milestone Project completed

--- udemy/milestone_projects/tictactoe.py
+++ udemy/milestone_projects/tictactoe.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 from __future__ import print_function
 
-import os
+import os, random
 
 def display_board(board):
 		
-		os.system('clear')
 		print('   |   |')
 		print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
 		print('   |   |')
@@ -18,5 +17,107 @@ def display_board(board):
 		print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
 		print('   |   |')
 
-board = ['-']*10
-display_board(board)
+def player_input():
+	marker = ''
+	while not (marker == 'O' or marker == 'X'):
+		marker = raw_input('Player 1: Do you want to be X or O? ').upper()
+	
+	if marker == 'X':
+		return ('X','O')
+	else:
+		return ('O','X')
+
+def place_marker(board, marker, position):
+	board[position] = marker
+
+def win_check(board,mark):
+	
+	return ((board[7] == mark and board[8] == mark and board[9] == mark) or
+	(board[4] == mark and board[5] == mark and board[6] == mark) or 
+	(board[1] == mark and board[2] == mark and board[3] == mark) or 
+	(board[7] == mark and board[4] == mark and board[1] == mark) or 
+	(board[8] == mark and board[5] == mark and board[2] == mark) or 
+	(board[9] == mark and board[6] == mark and board[3] == mark) or
+	(board[7] == mark and board[5] == mark and board[3] == mark) or
+	(board[9] == mark and board[5] == mark and board[1] == mark))
+	
+def choose_first():
+	if random.randint(0, 1) == 0:
+		return 'Player 1'
+	else:
+		return 'Player 2'
+
+def space_check(board, position):
+	
+	return board[position] == ' '
+
+def full_board_check(board):
+	for i in range (1,10):
+		if space_check(board, i):
+			return False
+	return True
+	
+def player_choice(board):
+	position = ' '
+	while position not in '1 2 3 4 5 6 7 8 9'.split() or not space_check(board,int(position)):
+		
+		position = raw_input('Choose your next position: (1-9) ')
+	return int(position)
+	
+def replay():
+	return raw_input('Do you want to play again? Enter Yes or No:').lower().startswith('y')
+	
+
+
+print ('Welcome to Tic Tac Toe!')
+	
+	
+while True:
+	theBoard = [' ']*10
+	display_board(theBoard)
+	player1_marker, player2_marker = player_input()
+	turn = choose_first()
+	print (turn + ' will go first')
+	
+	game_on = True
+		
+	while game_on:
+		
+		if turn == 'Player 1':
+			
+			display_board(theBoard)
+			position = player_choice(theBoard)
+			place_marker(theBoard, player1_marker, position)
+				
+			if win_check(theBoard, player1_marker):
+				display_board(theBoard)
+				print('Congratulations, Player 1 has won the game')
+				game_on = False
+			
+			else:
+				if full_board_check(theBoard):
+					display_board(theBoard)
+					print('The game is a draw!')
+					break
+				else:
+					turn = 'Player 2'
+		else:
+			#Player's 2 turn
+			display_board(theBoard)
+			position = player_choice(theBoard)
+			place_marker(theBoard, player2_marker, position)
+			
+			if win_check(theBoard, player2_marker):
+				display_board(theBoard)
+				print('Congratulations, Player 2 has won the game')
+				game_on = False
+			
+			else:
+				if full_board_check(theBoard):
+					display_board(theBoard)
+					print('The game is a draw!')
+					break
+				else:
+					turn = 'Player 1'
+	if not replay():
+		break
