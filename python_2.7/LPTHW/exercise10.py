c7630e728c37dbd3611b88b3b2964be4ae3c6644
Kunal Pamani <kunalpamani@kpmbp15personal.attlocal.net>
2017-02-19 16:54:36 -0800

    exercise10

--- python_2.7/LPTHW/exercise10.py
+++ python_2.7/LPTHW/exercise10.py
@@ -2,3 +2,32 @@
 
 ### Exercise 10 ###
 ### What Was That? ###
+
+# escape sequence
+print "I am 5'11\" tall." # escape double-quote inside string
+print 'I am 5\'11" tall.' # escape single-quote inside string
+
+tabby_cat = "\tI'm tabed in." # \t tabs in
+persian_cat = "I'm split\non a line." # \n for a new line
+backlash_cat = "I'm \\ a \\ cat." # \\ to show one '\'
+
+# example of multiline tabs
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backlash_cat
+print fat_cat
+
+'''
+Interesting piece of code that will never stop:
+
+while True :
+	for i in ["/" ,"-" ,"|" ,"\\" ,"|"]:
+		print "%s\r" % i ,
+'''
