979c301840a96f0180dd77b914bfea5fabf75532
Kunal Pamani <kpamani@apple.com>
2017-03-12 17:48:36 -0700

    exercise18.py

--- python_2.7/LPTHW/exercise18.py
+++ python_2.7/LPTHW/exercise18.py
@@ -3,3 +3,15 @@
 ### Exercise 18 ###
 ### Names, Variables, Code, Functions ###
 
+def print_two(*args):
+	arg1, arg2 = args
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# *args is pointless. Another way:
+def print_two_again(arg1, arg2):
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# This just takes one argument:
+def print_one(arg1):
+	print "arg1L %r" % arg1
+
