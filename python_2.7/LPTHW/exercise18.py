3ea5fd3f8108e3b70c43c5da9e0bfe78485d9817
Kunal Pamani <kunalpamani@kpmbp15personal.attlocal.net>
2017-03-13 01:20:09 -0700

    Up to exercise20.py

--- python_2.7/LPTHW/exercise18.py
+++ python_2.7/LPTHW/exercise18.py
@@ -13,5 +13,14 @@ def print_two_again(arg1, arg2):
 
 # This just takes one argument:
 def print_one(arg1):
-	print "arg1L %r" % arg1
+	print "arg1 %r" % arg1
 
+# This takes no arguments
+def print_none():
+	print "I got nothin'"
+
+
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
+print_one("First!")
+print_none()
